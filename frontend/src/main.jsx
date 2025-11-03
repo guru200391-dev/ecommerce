1293ae6889a3f64961e88461532ba21595e34733
guru200391-dev <guru200391@gmail.com>
2025-11-03 16:47:05 +0530

    ecommerce

---
+++ frontend/src/main.jsx
@@ -0,0 +1,36 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+import AdminRoute from "./pages/Admin/AdminRoute";
+import Profile from "./pages/User/Profile";
+import UserList from "./pages/Admin/UserList";
+import PrivateRoute from "./components/PrivateRoute";
+import CategoryList from "./pages/Admin/CategoryList";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+       <Route path="/login" element={<Login />} />
+       <Route path="/register" element={<Register />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+          <Route path="categorylist" element={<CategoryList />} />
+      </Route>
+    </Route>
+  ))
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <Provider store={store}>
+  <RouterProvider router={router} />
+  </Provider>
+);
